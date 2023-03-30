2cc2df97bda0d08c5a1d145774fedd3a7a44210a
Pedro Henrick <pedrohenrickqa@gmail.com>
2023-03-30 15:10:10 -0300

    feat: add autostart extend option

--- config/qtile/scripts/autostart.sh
+++ config/qtile/scripts/autostart.sh
@@ -1,8 +1,16 @@
 #!/bin/sh
 
+# autostart extend
+if [ -e ~/.autostart ];then
+ ~/.autostart
+fi
+
+# set wallpaper
 if [ -e ~/.fehbg ];then
   sh ~/.fehbg &
 else
   ~/.bin/set-background ~/Imagens/backgrounds/default.jpg
 fi
+
+# autostart apps
 picom -f & 
