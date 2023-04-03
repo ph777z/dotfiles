6638e85f8b8acebcf6af866b49ef7512dd6ab2a8
Pedro Henrick <pedrohenrickqa@gmail.com>
2023-04-03 00:49:29 -0300

    feat: auto install tmux tpm

--- scripts/install.sh
+++ scripts/install.sh
@@ -8,6 +8,9 @@ git clone https://github.com/pedrohenrick777/dotfiles.git $DOTFILES_PATH
 echo -e "\nInstalando dependencias\n"
 $DOTFILES_PATH/scripts/install_apps.py
 
+echo -e "\nClonando Tmux TPM\n"
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
 echo -e "\nCriando Links\n"
 $DOTFILES_PATH/scripts/create_links.sh
 
