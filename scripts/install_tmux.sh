d21375144da9a261190fedb1cc12f3029fc7e0c9
Pedro Henrick <pedrohenrickqa@gmail.com>
2023-04-03 01:04:34 -0300

    feat: auto install tmux plugins and tpm

---
+++ scripts/install_tmux.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo -e "\nClonando Tmux TPM\n"
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+echo -e "\nClonando tmux-theme\n"
+git clone https://github.com/jimeh/tmux-themepack.git ~/.tmux/plugins/tmux-themepack
+
+echo -e "\nClonando tmux-theme\n"
+git clone https://github.com/christoomey/vim-tmux-navigator.git ~/.tmux/plugins/vim-tmux-navigator
