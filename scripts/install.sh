6bc927e73230afe757699507de036a8e190e1a43
Pedro Henrick <pedrohenrickqa@gmail.com>
2023-04-03 11:58:33 -0300

    feat: complete install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -13,3 +13,18 @@ $DOTFILES_PATH/scripts/create_links.sh
 
 echo -e "\nSetando zsh como shell padrão\n"
 chsh -s /bin/zsh
+
+echo -e "\nAtivando servicos systemd\n"
+sudo systemctl enable docker.service
+sudo systemctl enable lightdm.service
+
+echo -e "\nPermissão de acesso ao docker\n"
+sudo usermod -aG docker $USER
+
+echo -e "\nReiniciar o sistema agora? [S/N] "
+read reiniciar
+
+case $reiniciar in
+  y*|Y*|s*|S*) reboot ;;
+  *) exit 0 ;;
+esac
