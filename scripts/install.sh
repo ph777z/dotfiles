aad3ad38ab08be6fa5502653034bb450bd01361b
Pedro Henrick <pedrohenrickqa@gmail.com>
2023-04-03 00:49:08 -0300

    fix: install script part install_apps

--- scripts/install.sh
+++ scripts/install.sh
@@ -6,7 +6,7 @@ echo -e "Clonando repositorio\n"
 git clone https://github.com/pedrohenrick777/dotfiles.git $DOTFILES_PATH
 
 echo -e "\nInstalando dependencias\n"
-$DOTFILES_PATH}/scripts/install_apps.py
+$DOTFILES_PATH/scripts/install_apps.py
 
 echo -e "\nCriando Links\n"
 $DOTFILES_PATH/scripts/create_links.sh
