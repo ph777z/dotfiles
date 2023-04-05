06a53c89c3591dc4332d3ca68c2d136fabf46282
Pedro Henrick <pedrohenrickqa@gmail.com>
2023-04-05 18:33:45 -0300

    fix: lvim install script

--- scripts/config_apps.sh
+++ scripts/config_apps.sh
@@ -26,6 +26,6 @@ fi
 
 
 # lvim
-if command -v lvim; then
+if [ ! -e $HOME/.local/bin/lvim ]; then
   LV_BRANCH='release-1.2/neovim-0.8' bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/fc6873809934917b470bff1b072171879899a36b/utils/installer/install.sh) --no-install-dependencies
 fi
