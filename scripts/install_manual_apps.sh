c7d98afbbb82a5fb14624d078fb83d8183061714
Pedro Henrick <pedrohenrickqa@gmail.com>
2023-04-24 15:48:07 -0300

    fix: install megasync

--- scripts/install_manual_apps.sh
+++ scripts/install_manual_apps.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 # megasync
-curl https://mega.nz/linux/repo/Arch_Extra/x86_64/megasync-x86_64.pkg.tar.zst > megasync.pkg.tar.zst
-sudo pacman -U --noconfirm megasync.pkg.tar.zst
-rm megasync.pkg.tar.zst
+pacman -Qi megasync &>/dev/null
+if [ ! $? -eq 0 ];then
+  curl https://mega.nz/linux/repo/Arch_Extra/x86_64/megasync-x86_64.pkg.tar.zst > megasync.pkg.tar.zst
+  sudo pacman -U --noconfirm megasync.pkg.tar.zst
+  rm megasync.pkg.tar.zst
+fi
