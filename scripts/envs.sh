0bd9a7ee856ca45e26d2fafdbe040aa38415f035
Pedro Henrick <pedrohenrickqa@gmail.com>
2023-08-15 00:36:44 -0300

    refactor: new structure

--- scripts/envs.sh
+++
@@ -1,8 +0,0 @@
-if [ ! -z $SUDO_USER ];then
-    export SCRIPT_USER=$SUDO_USER
-else
-    export SCRIPT_USER=$USER
-fi
-
-export DOTFILES_PATH=$HOME/.dotfiles
-export SCRIPTS_PATH=$DOTFILES_PATH/scripts
