2262a88890a5f88af7f1b86cafe25ddcd13d09dc
Pedro Henrick <pedrohenrickqa@gmail.com>
2023-08-17 19:25:54 -0300

    feat: add new installer script

--- install.sh
+++ install.sh
@@ -1,15 +1,159 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 set -e
 
-CONFIG="install.conf.yaml"
-DOTBOT_DIR="dotbot"
+sudo -v
 
-DOTBOT_BIN="bin/dotbot"
 BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+ASSETSDIR=$BASEDIR/assets
 
-cd "${BASEDIR}"
-git -C "${DOTBOT_DIR}" submodule sync --quiet --recursive
-git submodule update --init --recursive "${BASEDIR}"
+yay_apps=(
+  'betterlockscreen'
+  'devour'
+)
 
-"${BASEDIR}/${DOTBOT_DIR}/${DOTBOT_BIN}" -d "${BASEDIR}" -c "${CONFIG}" "${@}"
+pacman_apps=(
+  'bluez'
+  'btop'
+  'dunst'
+  'exa'
+  'fd'
+  'feh'
+  'firefox'
+  'flatpak'
+  'fzf'
+  'kitty'
+  'lazygit'
+  'lightdm'
+  'lightdm-gtk-greeter'
+  'mpv'
+  'neovim'
+  'nodejs'
+  'noto-fonts'
+  'noto-fonts-cjk'
+  'noto-fonts-emoji'
+  'npm'
+  'openssh'
+  'pacman-contrib'
+  'pamixer'
+  'pavucontrol'
+  'picom'
+  'pipewire'
+  'pipewire-alsa'
+  'pipewire-jack'
+  'pipewire-pulse'
+  'python'
+  'python-dbus-next'
+  'python-iwlib'
+  'python-pillow'
+  'python-pip'
+  'python-xlib'
+  'qtile'
+  'ranger'
+  'ripgrep'
+  'rofi'
+  'rust'
+  'starship'
+  'tk'
+  'tmux'
+  'ttf-jetbrains-mono'
+  'ttf-nerd-fonts-symbols'
+  'ttf-nerd-fonts-symbols-common'
+  'ttf-nerd-fonts-symbols-mono'
+  'unzip'
+  'wireplumber'
+  'xclip'
+  'xdg-desktop-portal-gtk'
+  'xorg-apps'
+  'xorg-server'
+  'zathura'
+  'zathura-pdf-mupdf'
+  'zip'
+  'zoxide'
+  'zsh'
+)
+
+yay_install() {
+  local YAYDIR=$BASEDIR/yay
+  if command -v yay 1>/dev/null 2>&1; then
+    git clone https://aur.archlinux.org/yay.git $YAYDIR\
+      && cd $YAYDIR \
+      && makepkg -si --noconfirm \
+      && cd $BASEDIR \
+      && rm -rf $YAYDIR
+  fi
+}
+
+config_lighdm() {
+  local BACKGROUNDSDIR=/usr/share/backgrounds/dotfiles
+  local LIGHTDMCONF=/etc/lightdm/lightdm-gtk-greeter.conf
+
+  if [ ! -d $BACKGROUNDSDIR ];then
+    sudo mkdir -p $BACKGROUNDSDIR
+  fi
+
+  sudo cp $ASSETSDIR/wallpaper.jpg $BACKGROUNDSDIR
+  sudo sed -i \
+    -e 's/#background=/background=\/usr\/share\/backgrounds\/dotfiles\/wallpaper.jpg/g' \
+    $LIGHTDMCONF
+  sudo systemctl enable lightdm.service
+}
+
+config_sudoers() {
+  local SUDOERSCONF=/etc/sudoers
+  sudo sed -i \
+    -e 's/# Defaults secure_path/Defaults secure_path/g' \
+    -e 's/# %wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) ALL/g' \
+    $SUDOERSCONF
+}
+
+config_npm() {
+  local NPMGLOBALDIR=$HOME/.npm-global
+
+  if [ ! -d $NPMGLOBALDIR ];then
+    mkdir $NPMGLOBALDIR
+  fi
+  npm config set prefix '~/.npm-global'
+}
+
+config_profile() {
+  local PROFILEPATH=$HOME/.profile
+
+  insert_in_profile() {
+    grep -qxF "${1}" $PROFILEPATH \
+      || echo $1 >> $PROFILEPATH
+  }
+
+  if [ ! -f $PROFILEPATH ];then
+    touch $PROFILEPATH
+  fi
+  insert_in_profile "export TERMINAL=kitty"
+  insert_in_profile "export EDITOR=nvim"
+}
+
+dotbot_install() {
+  local CONFIG="install.conf.yaml"
+  local DOTBOT_DIR="dotbot"
+  local DOTBOT_BIN="bin/dotbot"
+
+  cd "${BASEDIR}"
+  git -C "${DOTBOT_DIR}" submodule sync --quiet --recursive
+  git submodule update --init --recursive "${BASEDIR}"
+
+  "${BASEDIR}/${DOTBOT_DIR}/${DOTBOT_BIN}" -d "${BASEDIR}" -c "${CONFIG}" "${@}"
+}
+
+
+yay_install
+
+sudo pacman -S --needed --noconfirm $(printf " %s" "${pacman_apps[@]}")
+yay -S --needed --noconfirm $(printf " %s" "${yay_apps[@]}")
+
+config_lighdm
+config_sudoers
+config_profile
+
+sudo chsh -s /bin/zsh $USER
+systemctl enable --user pipewire.service
+
+dotbot_install
