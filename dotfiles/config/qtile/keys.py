1abd300564d408ba6c0c0358dcd12964e528960d
Pedro Henrick <pedrohenrickqa@gmail.com>
2023-08-16 22:45:19 -0300

    refactor: qtile configs

---
+++ dotfiles/config/qtile/keys.py
@@ -0,0 +1,69 @@
+from pathlib import Path
+
+from libqtile.config import Key, KeyChord, Click, Drag
+from libqtile.lazy import lazy
+
+from . import terminal, rofi_menu, rofi_window, SCRIPTS_PATH
+from screens import screens_obj
+
+
+window_key = 'mod4'
+alt_key = 'mod1'
+
+keys = [
+    KeyChord([window_key], 'o', [
+        Key([], 'h', lazy.layout.left()),
+        Key([], 'l', lazy.layout.right()),
+        Key([], 'j', lazy.layout.down()),
+        Key([], 'k', lazy.layout.up()),
+        Key(['shift'], 'h', lazy.layout.shuffle_left()),
+        Key(['shift'], 'l', lazy.layout.shuffle_right()),
+        Key(['shift'], 'j', lazy.layout.shuffle_down()),
+        Key(['shift'], 'k', lazy.layout.shuffle_up()),
+        Key(['control'], 'h', lazy.layout.grow_left()),
+        Key(['control'], 'l', lazy.layout.grow_right()),
+        Key(['control'], 'j', lazy.layout.grow_down()),
+        Key(['control'], 'k', lazy.layout.grow_up()),
+        Key([], 'n', lazy.layout.normalize()),
+        Key(['shift'], 'Return', lazy.layout.toggle_split())],
+        mode=True,
+        name='window'
+    ),
+
+    *[Key([alt_key], g.name, lazy.group[g.name].toscreen()) for g in screens_obj.groups],
+    *[Key([window_key, 'shift'], g.name, lazy.window.togroup(g.name, switch_group=True)) for g in screens_obj.groups],
+
+    Key([alt_key], 'r', lazy.reload_config()),
+    Key([alt_key], 'q', lazy.spawn(f'sh {Path(SCRIPTS_PATH, "powermenu.sh")}')),
+    #Key([window_key, 'control'], 'Tab', lazy.next_layout()),
+    Key([window_key], 'w', lazy.window.kill()),
+    Key([window_key], 'x', lazy.spawn('kitty bash -c "xprop;read"')),
+    Key([window_key], 'g', lazy.window.toggle_floating()),
+
+    Key([], 'XF86AudioLowerVolume', lazy.spawn('pamixer --decrease 5')),
+    Key([], 'XF86AudioRaiseVolume', lazy.spawn('pamixer --increase 5')),
+    Key([], 'XF86AudioMute', lazy.spawn('pamixer --toggle-mute')),
+    
+    KeyChord([alt_key], 'shift', [
+        Key([], 'Space', lazy.spawn(rofi_menu)),
+        Key([], 'Return', lazy.spawn(terminal))],
+    ),
+    Key([window_key], 'Tab', lazy.spawn(rofi_window))
+]
+
+# Drag floating layouts.
+mouse = [
+    Drag(
+        [window_key],
+        'Button1',
+        lazy.window.set_position_floating(),
+        start=lazy.window.get_position(),
+    ),
+    Drag(
+        [window_key],
+        'Button3',
+        lazy.window.set_size_floating(),
+        start=lazy.window.get_size(),
+    ),
+    Click([window_key], 'Button1', lazy.window.bring_to_front()),
+]
---
+++ dotfiles/config/qtile/layouts.py
@@ -0,0 +1,37 @@
+from libqtile.config import Match
+from libqtile.layout import floating, bsp, max
+
+from . import theme
+
+
+layouts = [
+    bsp.Bsp(
+        border_width=1,
+        margin=6,
+        border_on_single=True,
+        border_focus=theme['white1'],
+        border_normal=theme['black2'],
+    ),
+    max.Max()
+]
+
+floating_layout = floating.Floating(
+    border_width=1,
+    border_focus=theme['white1'],
+    border_normal=theme['black2'],
+    float_rules=[
+        # defaults
+        Match(wm_class='confirmreset'),  # gitk
+        Match(wm_class='makebranch'),  # gitk
+        Match(wm_class='maketag'),  # gitk
+        Match(wm_class='ssh-askpass'),  # ssh-askpass
+        Match(title='branchdialog'),  # gitk
+        Match(title='pinentry'),  # GPG key password entry
+        # customs
+        Match(title='Salvar como'),
+        Match(title='Discord Updater'),
+        Match(wm_class='Pavucontrol'),
+        Match(wm_class='Anydesk'),
+        Match(wm_class='megasync'),
+    ],
+)
