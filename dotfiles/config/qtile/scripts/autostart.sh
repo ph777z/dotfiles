c9b4e0570530312642bfc7b81a9f108a4fbf6834
Pedro Henrick <pedrohenrickqa@gmail.com>
2023-08-28 20:25:07 -0300

    refactor: fehbg selector in autostart

--- dotfiles/config/qtile/scripts/autostart.sh
+++ dotfiles/config/qtile/scripts/autostart.sh
@@ -3,11 +3,11 @@
 # set wallpaper
 WALLPAPERPATH=/usr/share/backgrounds/dotfiles/wallpaper.jpg
 
-if [ ! -e ~/.fehbg ];then
+if [ ! -e $HOME/.fehbg ];then
     feh --no-fehbg \
         --bg-scale $WALLPAPER_PATH
 else
-    $HOME/.fehbg &
+    $HOME/.fehbg
 fi
 
 # autostart apps
