f45160542669c92215c011b7e11fde9ab720b352
Pedro Henrick <pedrohenrickqa@gmail.com>
2023-09-14 18:56:00 -0300

    feat: add firefox configs

---
+++ dotfiles/firefox/user.js
@@ -0,0 +1,9 @@
+// enable stylesheets
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+
+// disable pocket
+user_pref("extensions.pocket.enabled", false);
+
+// disable firefox view and tabs manager
+user_pref("browser.tabs.firefox-view", false);
+user_pref("browser.tabs.tabmanager.enabled", false);
